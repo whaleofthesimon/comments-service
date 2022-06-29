9ff1f71bc813ee03b461d66689e9823beebc3435
whaleofthesimon <snv.whale@gmail.com>
2022-06-29 10:27:37 +0300

    Refactored model with MPTT library usage, added temporary test models like Post and Blog

--- comm_app/admin.py
+++ comm_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from comm_app.models import Comment, Post, Blog
 
-from comm_app.models import Commentaries
-
-admin.site.register(Commentaries)
+admin.site.register(Comment, MPTTModelAdmin)
+admin.site.register(Post)
+admin.site.register(Blog)
