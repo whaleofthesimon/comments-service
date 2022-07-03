8e4a063b76a2335bcb49f5528317af0499e8e806
whaleofthesimon <snv.whale@gmail.com>
2022-07-03 16:30:00 +0300

    Fully refactored base viewset: removed logic, url_path and excess actions; added get_queryset and perform_create

--- comm_app/views.py
+++ comm_app/views.py
@@ -1,6 +1,3 @@
-import csv
-
-from django.http import HttpResponse
 from rest_framework import mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -8,93 +5,39 @@ from rest_framework.viewsets import GenericViewSet
 
 from comm_app.models import *
 from comm_app.serializers import AllCommentsSerializer
+from comm_app.services import *
 
 
 class CommentsViewSet(mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
                       GenericViewSet):
     serializer_class = AllCommentsSerializer
-    queryset = Commentaries.objects.all()
 
-    @action(detail=False, methods=['get'], url_path=r'comm_for_entity/(?P<entity_name>\w+)/(?P<object_id>\w+)')
-    def comm_for_entity(self, request, entity_name, object_id):
-        comm_list = Commentaries.objects.filter(content_type__model=entity_name, object_id=object_id)
-        paged_comm_list = self.paginate_queryset(comm_list)
-        if paged_comm_list is not None:
-            serializer = self.get_serializer(paged_comm_list, many=True)
-            return self.get_paginated_response(serializer.data)
+    def get_queryset(self, export=False):
+        entity_name = self.request.query_params.get('entity')
+        object_id = self.request.query_params.get('id')
+        username = self.request.query_params.get('username')
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
 
-        serializer = self.get_serializer(comm_list, many=True)
-        return Response(serializer.data)
+        return get_queryset_filter(entity_name, object_id, username, start_date, end_date, export)
 
-    @action(detail=False, methods=['get'], url_path=r'comm_branch/(?P<entity_name>\w+)/(?P<object_id>\w+)')
-    def comm_branch(self, request, entity_name, object_id):
-        comm_list = []
-
-        def get_comm_list(entity_name, object_id):
-            query_request = Commentaries.objects.filter(content_type__model=entity_name, object_id=object_id)
-            if query_request:
-                comm_list.extend(list(query_request))
-            for query in query_request:
-                get_comm_list(entity_name='commentaries', object_id=query.id)
-
-        get_comm_list(entity_name, object_id)
-        serializer = self.get_serializer(comm_list, many=True)
-        return Response(serializer.data)
-
-    @action(detail=False, methods=['get'], url_path=r'comm_by_user/(?P<username>\w+)')
-    def comm_by_user(self, request, username):
-        comm_list = Commentaries.objects.filter(author__username=username)
-        serializer = self.get_serializer(comm_list, many=True)
-        return Response(serializer.data)
-
-
-class CommExport(GenericViewSet):
-
-    @staticmethod
-    def get_csv(query_set):
-        output = []
-        response = HttpResponse(content_type='text/csv')
-        response.write(u'\ufeff'.encode('utf8'))
-        writer = csv.writer(response)
-        writer.writerow(['Author', 'Content', 'Creation Date', 'Related Entity', 'Object ID'])
-        for comment in query_set:
-            output.append([comment.author,
-                           comment.content,
-                           comment.creation_date,
-                           comment.content_type.model,
-                           comment.object_id])
-        writer.writerows(output)
-        return response
-
-    @action(detail=False, url_path=r'get_csv/(?P<username>\w+)'
-                                   r'(/(?P<start_date>[\w-]+)/(?P<end_date>[\w-]+))*')
-    def get_csv_by_user(self, request, username, start_date=None, end_date=None):
-        if start_date and end_date:
-            query_set = Commentaries.objects.filter(author__username=username,
-                                                    creation_date__lte=end_date,
-                                                    creation_date__gte=start_date)
+    def perform_create(self, serializer):  # Autocomplete of "Parent" field by content_type and id
+        content_type_id = self.request.data.get('content_type')
+        object_id = self.request.data.get('object_id')
+        if ContentType.objects.get(id=content_type_id).model == 'comment':
+            parent = Comment(pk=object_id)
         else:
-            query_set = Commentaries.objects.filter(author__username=username)
-        return CommExport.get_csv(query_set)
+            parent = None
+        serializer.save(parent=parent)
 
-    @action(detail=False, url_path=r'get_csv/(?P<entity_name>\w+)/(?P<object_id>\w+)'
-                                   r'(/(?P<start_date>[\w-]+)/(?P<end_date>[\w-]+))*')
-    def get_csv_by_entity(self, request, entity_name, object_id, start_date=None, end_date=None):
-        query_set = []
+    @action(detail=False, methods=['get'])
+    def branch(self, request):
+        branch = self.get_queryset().get_descendants(include_self=True)
+        serializer = self.get_serializer(branch, many=True)
+        return Response(serializer.data)
 
-        def get_query_set(entity_name, object_id):
-            if start_date and end_date:
-                query_request = Commentaries.objects.filter(content_type__model=entity_name,
-                                                            object_id=object_id,
-                                                            creation_date__lte=end_date,
-                                                            creation_date__gte=start_date)
-            else:
-                query_request = Commentaries.objects.filter(content_type__model=entity_name,
-                                                            object_id=object_id)
-            if query_request:
-                query_set.extend(list(query_request))
-            for query in query_request:
-                get_query_set(entity_name='commentaries', object_id=query.id)
-
-        get_query_set(entity_name, object_id)
-        return CommExport.get_csv(query_set)
+    @action(detail=False, methods=['get'])
+    def csv_export(self, request):
+        exporting_queryset = self.get_queryset(export=True)
+        return export_to_csv(exporting_queryset)
