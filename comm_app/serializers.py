4b8ca26c5e838a3e6093fcb1a0c75d897de666a3
whaleofthesimon <snv.whale@gmail.com>
2022-07-03 17:13:45 +0300

    Added validation for content_type and object_id fields by existance of current object; added fields of MPTT library

--- comm_app/serializers.py
+++ comm_app/serializers.py
@@ -3,6 +3,16 @@ from comm_app.models import *
 
 
 class AllCommentsSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        if data['content_type'] not in ContentType.objects.all():
+            raise serializers.ValidationError("This entity doesn't exist yet!")
+        if not data['content_type'].model_class().objects.filter(id=data['object_id']):
+            raise serializers.ValidationError("This entity's object doesn't exist yet!")
+        return data
+
     class Meta:
-        model = Commentaries
-        fields = ('author', 'content', 'creation_date', 'content_type', 'object_id')
+        model = Comment
+        fields = (
+            'author', 'content', 'creation_date', 'content_type', 'object_id', 'parent', 'level', 'lft', 'rght',
+            'tree_id',)
+        read_only_fields = ('parent', 'level', 'lft', 'rght', 'tree_id',)
