6aec2ce355a2e7220b7f9d8ceb720494da417f2d
whaleofthesimon <snv.whale@gmail.com>
2022-07-04 13:04:21 +0300

    Removed comm from URL path, current URL suffix: api/v1/commentaries/...

--- commentsAPI/urls.py
+++ commentsAPI/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/comm/', include('comm_app.urls')),
+    path('api/v1/', include('comm_app.urls')),
 ]
