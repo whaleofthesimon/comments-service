62e33dbd5f7f96d8ff6a6f5c4e77acb356541cc3
whaleofthesimon <snv.whale@gmail.com>
2022-07-05 20:28:30 +0300

    Added django-filter library, filter_service fully refactored with FilterSets

--- comm_app/services/filter_service.py
+++ comm_app/services/filter_service.py
@@ -1,25 +1,28 @@
 from comm_app.models import Comment
+from django_filters import rest_framework as filters
 
 
-def get_queryset_filter(entity_name, object_id, username, start_date, end_date, export):
-    if entity_name and object_id:
-        if entity_name == 'comment':
-            queryset = Comment.objects.filter(id=object_id)
-        else:
-            queryset = Comment.objects.filter(content_type__model=entity_name, object_id=object_id)
-        if export:
-            branch_queryset = queryset.get_descendants(include_self=True)
-            if start_date and end_date:
-                return branch_queryset.filter(creation_date__lte=end_date, creation_date__gte=start_date)
-            return branch_queryset
-        return queryset
+class CommonFilter(filters.FilterSet):
+    entity = filters.CharFilter(field_name='content_type__model')
+    id = filters.NumberFilter(field_name='object_id')
+    username = filters.CharFilter(field_name='author__username')
 
-    elif username:
-        if start_date and end_date:
-            return Comment.objects.filter(author__username=username,
-                                          creation_date__lte=end_date,
-                                          creation_date__gte=start_date)
-        return Comment.objects.filter(author__username=username)
+    class Meta:
+        model = Comment
+        fields = ['author__username', 'content_type__model', 'object_id']
 
-    else:
-        return Comment.objects.all()
+
+class OnlyCommentFilter(filters.FilterSet):
+    id = filters.NumberFilter(field_name='id')
+
+    class Meta:
+        model = Comment
+        fields = ['id']
+
+
+class BetweenDateFilter(filters.FilterSet):
+    date = filters.DateFromToRangeFilter(field_name='creation_date')
+
+    class Meta:
+        model = Comment
+        fields = ['creation_date']
--- comm_app/views.py
+++ comm_app/views.py
@@ -2,25 +2,28 @@ from rest_framework import mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from django_filters import rest_framework as filters
 
 from comm_app.models import *
 from comm_app.serializers import AllCommentsSerializer
-from comm_app.services import export_service, filter_service
+from comm_app.services import export_service
+from comm_app.services.filter_service import CommonFilter, OnlyCommentFilter, BetweenDateFilter
 
 
 class CommentsViewSet(mixins.CreateModelMixin,
                       mixins.ListModelMixin,
                       GenericViewSet):
     serializer_class = AllCommentsSerializer
+    queryset = Comment.objects.all()
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = CommonFilter
 
-    def get_queryset(self, export=False):
-        entity_name = self.request.query_params.get('entity')
-        object_id = self.request.query_params.get('id')
-        username = self.request.query_params.get('username')
-        start_date = self.request.query_params.get('start_date')
-        end_date = self.request.query_params.get('end_date')
-
-        return filter_service.get_queryset_filter(entity_name, object_id, username, start_date, end_date, export)
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.query_params.get('entity') == 'comment':
+            self.filterset_class = OnlyCommentFilter
+        self.filter = self.filterset_class(self.request.GET, queryset=qs)
+        return self.filter.qs
 
     def perform_create(self, serializer):  # Autocomplete of "Parent" field by content_type and id
         content_type_id = self.request.data.get('content_type')
@@ -39,5 +42,9 @@ class CommentsViewSet(mixins.CreateModelMixin,
 
     @action(detail=False, methods=['get'])
     def csv_export(self, request):
-        exporting_queryset = self.get_queryset(export=True)
+        if self.request.query_params.get('entity') and self.request.query_params.get('id'):
+            exporting_queryset = self.get_queryset().get_descendants(include_self=True)
+        else:
+            exporting_queryset = self.get_queryset()
+        exporting_queryset = BetweenDateFilter(self.request.GET, queryset=exporting_queryset).qs
         return export_service.export_to_csv(exporting_queryset)
