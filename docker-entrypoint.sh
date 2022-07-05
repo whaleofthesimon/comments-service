83799c3f6c7a82fa6955e823c7fcd2267f84d6ad
whaleofthesimon <snv.whale@gmail.com>
2022-07-05 21:40:01 +0300

    Added gunicorn server runner

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,5 @@ python manage.py migrate
 
 python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 
-python manage.py runserver 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
+gunicorn --bind 0.0.0.0:8000 commentsAPI.wsgi
